2b6ae1378abdcbb5f7e245b0054ccddcd7c9ee49
nguyenq <nguyenq@5968eacb-d6fe-4cef-bb96-d5b655664892>
2013-10-15 13:27:57 +0000

    Implement a FixedSizeStack

--- GUIWithImage.cs
+++ GUIWithImage.cs
@@ -254,31 +254,4 @@ namespace VietOCR.NET
             regkey.SetValue(strScreenshotMode, Convert.ToInt32(this.screenshotModeToolStripMenuItem.Checked));
         }
     }
-
-    class FixedSizeStack<T> : LinkedList<T> 
-    {
-        private int limit;
-
-        public FixedSizeStack(int limit) : base()
-        {
-            this.limit = limit;
-        }
-
-        public T Pop()
-        {
-            T obj = base.First.Value;
-            base.RemoveFirst();
-            return obj;
-        }
-
-        public void Push(T obj)
-        {
-            base.AddFirst(obj);
-            if (this.Count > limit)
-            {
-                base.RemoveLast();
-            }
-        }
-
-    }
 }
---
+++ Utilities/FixedSizeStack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VietOCR.NET.Utilities
+{
+    public class FixedSizeStack<T> : LinkedList<T>
+    {
+        private int limit;
+
+        public FixedSizeStack(int limit)
+            : base()
+        {
+            this.limit = limit;
+        }
+
+        public T Pop()
+        {
+            T obj = base.First.Value;
+            base.RemoveFirst();
+            return obj;
+        }
+
+        public void Push(T obj)
+        {
+            base.AddFirst(obj);
+            if (this.Count > limit)
+            {
+                base.RemoveLast();
+            }
+        }
+    }
+}
