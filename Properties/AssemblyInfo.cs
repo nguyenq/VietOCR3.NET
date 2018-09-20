fdf4453eb9675c98aad2e2653fe3f40fe8cbdb2f
nguyenq <nguyenq@users.sf.net>
2018-09-20 06:47:46 -0500

    Prepare for release

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("4.4.0.0")]
+[assembly: AssemblyFileVersion("4.7.1.0")]
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("TestProject")]
